4f9e515265f4706ac2b7ac1c2e49f656d042d690
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-30 12:47:44 -0500

    [php]: Initial commit

---
+++ comments.php
@@ -0,0 +1,33 @@
+<?php
+
+// Include database credentials from config.php
+require_once('config.php');
+
+// Connect to MySQL
+$mysqli = new mysqli($host, $username, $password, $database, $port);
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+// Perform the database query
+$query = "SELECT * FROM comments";
+$result = $mysqli->query($query);
+
+// Prepare data for JSON encoding
+$data = array();
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    $result->free();
+} else {
+    echo "Error executing query: " . $mysqli->error;
+}
+
+// Close the MySQL connection
+$mysqli->close();
+
+// Send data as JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
